73b97ec695da2cbd5eb94f74b3fb02bc7b6d9729
Alexander <Sanya19976@gmail.com>
2022-04-18 14:17:54 +0300

    Parser site ToppNorsk

---
+++ main.py
@@ -0,0 +1,46 @@
+#Парсер сайта Toppnorsk для удобного сохранения слов в файлы для последующей загрузки в quizlet
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+class Driver:
+#Инициализируем драйвер для работы с сайтом
+    def __init__(self):
+        self.options = Options()
+        self.options.add_argument('--no-sandbox')
+        self.driver = webdriver.Chrome(options=self.options)
+#Добавляем переменные для работы с сайтом и со словами
+        self.site = None
+        self.wordsList = {}
+        self.word = None
+        self.definition = None
+#Переход на сайт.
+    def goToTheSite(self,site):
+        self.driver.get(site)
+
+#Находим слова и сохраняем в словарь
+    def closrSite(self):
+        self.driver.close()
+    def findWords(self):
+        try:
+            for i in range(1000):
+                try:
+                    self.word=(self.driver.find_element(By.XPATH,f'/html/body/div[1]/div/div[1]/main/article/div/div/p[{i+2}]/strong').text)
+                    self.definition=(self.driver.find_element(By.XPATH,f'/html/body/div[1]/div/div[1]/main/article/div/div/p[{i+2}]').text)
+                    self.definition.replace(self.word,'')
+                    self.wordsList[self.word]==self.definition
+                    print(self.word)
+                    print(self.definition)
+                    print(self.wordsList)
+                except:
+                    break
+        except:
+            print("I can't find elements")
+
+    def printWordList(self):
+        print(self.wordsList)
+
+fixture = Driver();
+fixture.goToTheSite("https://toppnorsk.com/2021/10/24/stor-samling-norske-uttrykk/")
+fixture.findWords()
+fixture.printWordList()
+fixture.closrSite()
